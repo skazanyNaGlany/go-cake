0bc1515d824e7c46884b43f13167a97ea5d9c419
Pawel <screamingbox@gmail.com>
2024-02-01 22:52:34 +0100

    Save

--- bases.go
+++ bases.go
@@ -11,3 +11,27 @@ func (bgkm *BaseGoKateModel) GetHTTPError() HTTPError {
 func (bgkm *BaseGoKateModel) SetHTTPError(httpError HTTPError) {
 	bgkm.gk_GetHTTPError = httpError
 }
+
+func (bgkm *BaseGoKateModel) CreateInstance() GoKateModel {
+	panic("not implemented")
+}
+
+func (bgkm *BaseGoKateModel) GetID() any {
+	panic("not implemented")
+}
+
+func (bgkm *BaseGoKateModel) SetID(id string) error {
+	panic("not implemented")
+}
+
+func (bgkm *BaseGoKateModel) CreateETag() any {
+	panic("not implemented")
+}
+
+func (bgkm *BaseGoKateModel) GetETag() any {
+	panic("not implemented")
+}
+
+func (bgkm *BaseGoKateModel) SetETag(etag string) error {
+	panic("not implemented")
+}
